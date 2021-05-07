f476d9902cd070de28f6f8cf9a8892d41a03ede7
carolinecole25 <carolinecole25@gmail.com>
2021-05-07 19:58:15 +0000

    contact page marker added to map

--- assets/js/contact-map.js
+++ assets/js/contact-map.js
@@ -3,9 +3,25 @@
 
 // credit: code taken from Code Institue lesson and edited to suit project, linked in README.md 
 
+ // function initMap() {
+    //         var map = new google.maps.Map(document.getElementById("map"), {
+      //           zoom: 18,
+       //          center: { lat: 50.086221, lng: -5.310867 }
+         //    });
+           //  const marker = new google.maps.Marker({
+             //   position: { lat: 50.086221, lng: -5.310867 }
+              //  map: map,
+               // });
+      //  }
+
 function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 18,
-                center: { lat: 50.086221, lng: -5.310867 }
-            });
-        }
+    const myLatLng = { lat: 50.086221, lng: -5.310867 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 18,
+        center: myLatLng,
+    });
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+    });
+}

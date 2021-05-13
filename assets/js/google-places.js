cc16ff839b44559933f460ec5d6efcab6ef7b2ae
carolinecole25 <carolinecole25@gmail.com>
2021-05-13 15:41:58 +0000

    explore styling changes

--- assets/js/google-places.js
+++ assets/js/google-places.js
@@ -2,9 +2,9 @@
 
 
 function initMap() {
-    const myLatLng = { lat: 50.086221, lng: -5.310867 };
+    const myLatLng = { lat: 50.08533653517827, lng: -5.316203635722799 };
     const map = new google.maps.Map(document.getElementById("map-explore"), {
-        zoom: 18,
+        zoom: 16,
         center: myLatLng,
     });
     new google.maps.Marker({
